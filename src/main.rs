5ff7502acf4b484598906f39debd081e6adc9bd0
Thiago Braz <thiagobrazpinheiro@gmail.com>
2026-05-31 17:58:11 -0300

    refactor: extract main logic into new function run

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ fn main() {
 
     println!("Searching for {} in file {}", config.query, config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.file_path)
             .expect("Shoul have been able to read the file!");
 

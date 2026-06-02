c1bac6b253b3e7fd00f01534ba106ae733706a1d
Thiago Braz <thiagobrazpinheiro@gmail.com>
2026-06-02 19:05:05 -0300

    refactor: implement standard error to error output

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Searching for {} in file {}", config.query, config.file_path);
 
     if let Err(err) = run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     }
 }
